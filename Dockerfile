21866963bde439b1f4a41287880d20817ebdbb27
935057137@qq.com <935057137@qq.com>
2019-10-30 14:55:27 +0800

    update

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM centos:7
+MAINTAINER Yeild
+LABEL maintainer="CentOS 7[vps] - Yeild"
+
+RUN curl -fsSL "https://raw.githubusercontent.com/yeild-docker/centos7/master/init.sh" | sh
+
+ENTRYPOINT [ "/usr/sbin/init" ]
+
---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+yum provides '*/applydeltarpm' && yum install deltarpm -y
+cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
+yum update -y
+cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
+
+# timezone
+curl -fsSL "https://raw.githubusercontent.com/yeild-docker/resource/master/centos7/locale/init.sh" | sh
+cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
+
+# sshd
+curl -fsSL "https://raw.githubusercontent.com/yeild-docker/resource/master/centos7/sshd/init.sh" | sh
+cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
+
+#nginx
+curl -fsSL "https://raw.githubusercontent.com/yeild-docker/resource/master/centos7/nginx/init.sh" | sh
+cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
+
+echo "source /etc/profile" >> /root/.bashrc && source /root/.bashrc
+
+yum clean all && rm -rf /var/cache/yum/*
+
+echo "Done"
+exit 0
